49e591205a5d73e135a80e8ac5a55896390b6a11
Xalilov <eljjey@gmail.com>
2021-02-06 19:24:08 +0500

    timezone

--- namangan.py
+++ namangan.py
@@ -1,11 +1,13 @@
 import requests
 from pprint import pprint
 api_key = "f6ce8813b4cfca0fc8036e602d4b051b"
-lat=41.3082
-lon=-69.2598
-url = f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}exclude=current&appid={api_key}'
+lat=40.9845254939922
+lon=72.0620398798127
+units = 'metric'
+url = f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units={units}&exclude=current&appid={api_key}'
 
     # https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 r=requests.get(url)
+data=r.json()
 
-pprint(r.json())
+pprint(data['timezone'])
